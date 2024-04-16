37e329e74e0aa7168e5ef3ef66c7466bccb3876a
Đức Lê <leduchvt@gmail.com>
2024-04-16 09:50:52 +0700

    Thay đổi form bắt đầu

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace BTL_WINFORM_2024
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new QuanLyKhachHang());
+            Application.Run(new QuanLyHangHoa());
         }
     }
 }
